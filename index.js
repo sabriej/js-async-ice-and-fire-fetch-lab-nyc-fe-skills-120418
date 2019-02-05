3b1b6c40b2e49c500dc341d38556b73736cbc0e5
Jennifer Gaillard <jengaillard1@gmail.com>
2019-02-05 23:43:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function fetchBooks() {
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});
